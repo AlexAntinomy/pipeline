08ae538cbd130d1d2622fcc61e4d858ee3e362c5
Антимонов Александр Викторович <AntimonovAV@mguu.ru>
2025-01-27 16:01:17 +0300

    added logging feature

--- go.go
+++ go.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"container/ring"
 	"fmt"
+	"log"
 	"os"
 	"strconv"
 	"strings"
@@ -26,8 +27,12 @@ func (s *NegativeFilterStage) Process(input <-chan int) <-chan int {
 	go func() {
 		defer close(output)
 		for num := range input {
+			log.Printf("NegativeFilterStage: получено число %d", num)
 			if num >= 0 {
+				log.Printf("NegativeFilterStage: число %d прошло фильтр", num)
 				output <- num
+			} else {
+				log.Printf("NegativeFilterStage: число %d отфильтровано", num)
 			}
 		}
 	}()
@@ -41,8 +46,12 @@ func (s *SpecialFilterStage) Process(input <-chan int) <-chan int {
 	go func() {
 		defer close(output)
 		for num := range input {
+			log.Printf("SpecialFilterStage: получено число %d", num)
 			if num != 0 && num%3 == 0 {
+				log.Printf("SpecialFilterStage: число %d прошло фильтр", num)
 				output <- num
+			} else {
+				log.Printf("SpecialFilterStage: число %d отфильтровано", num)
 			}
 		}
 	}()
@@ -69,11 +78,14 @@ func (s *BufferStage) Process(input <-chan int) <-chan int {
 				if !ok {
 					return
 				}
+				log.Printf("BufferStage: добавлено число %d в буфер", num)
 				s.buffer.Value = num
 				s.buffer = s.buffer.Next()
 			case <-ticker.C:
+				log.Println("BufferStage: сработал таймер, выгрузка буфера")
 				s.buffer.Do(func(value interface{}) {
 					if value != nil {
+						log.Printf("BufferStage: выгружено число %d из буфера", value.(int))
 						output <- value.(int)
 					}
 				})
@@ -84,6 +96,9 @@ func (s *BufferStage) Process(input <-chan int) <-chan int {
 }
 
 func main() {
+	log.SetFlags(log.LstdFlags | log.Lmicroseconds)
+	log.Println("Программа запущена")
+
 	dataSource := func() (<-chan int, chan struct{}) {
 		c := make(chan int)
 		done := make(chan struct{})
@@ -93,15 +108,17 @@ func main() {
 			for scanner.Scan() {
 				input := scanner.Text()
 				if strings.EqualFold(input, "exit") {
-					fmt.Println("Программа завершила работу!")
+					log.Println("Программа завершила работу по команде 'exit'")
 					close(done)
 					return
 				}
 				num, err := strconv.Atoi(input)
 				if err != nil {
+					log.Printf("Ошибка ввода: %v", err)
 					fmt.Println("Программа обрабатывает только целые числа!")
 					continue
 				}
+				log.Printf("DataSource: получено число %d", num)
 				c <- num
 			}
 		}()
@@ -113,10 +130,13 @@ func main() {
 			select {
 			case num, ok := <-input:
 				if !ok {
+					log.Println("Consumer: канал закрыт, завершение работы")
 					return
 				}
+				log.Printf("Consumer: обработаны данные: %d", num)
 				fmt.Printf("Обработаны данные: %d\n", num)
 			case <-done:
+				log.Println("Consumer: получен сигнал завершения, завершение работы")
 				return
 			}
 		}
@@ -135,4 +155,5 @@ func main() {
 	}
 
 	consumer(pipeline, done)
+	log.Println("Программа завершена")
 }
